55bee21297d0cd03da7821a884eac057a960f6e3
unknown <nghiazexal@gmail.com>
2021-11-08 10:31:17 +0700

    hash crud

---
+++ src/test/java/com/example/crud_redis_tmn/CrudRedisTmnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_tmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisTmnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
